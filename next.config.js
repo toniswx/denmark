e1f8d7246d86be48ae660c9ca8a491243d58061d
Antonio <toninhoport@gmail.com>
2023-12-30 10:00:08 -0300

    ignore build errors

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
     images: {
         domains: ['images.unsplash.com'],
+      },
+      eslint: {
+        ignoreDuringBuilds: true,
+      },
+      reactStrictMode: true,
+      typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
       },
 }
 
